dc8ea997f5ea27d6fd42cd4ed74993d7b22f2a33
Nicholas Tickle <nticklesa@gmail.com>
2025-10-11 13:17:27 +0100

    Updated the handles for the task cards

--- components/task-card/task-handles.tsx
+++ components/task-card/task-handles.tsx
@@ -7,13 +7,13 @@ export default function TaskHandles() {
             type="target" 
             position={Position.Left} 
             id="in" 
-            className='!w-4 !h-4 !border-none !bg-task-card-handles-background !-left-[15px]'
+            className='!w-6 !h-6 !border-none !bg-task-card-handles-background !-left-[15px] hover:!cursor-pointer '
             />
             <Handle 
             type="source" 
             position={Position.Right} 
             id="out" 
-            className='!w-4 !h-4 !border-none !bg-task-card-handles-background !-right-[15px] '
+            className='!w-6 !h-6 !border-none !bg-task-card-handles-background !-right-[15px] hover:!cursor-pointer'
             />
         </>
     );
